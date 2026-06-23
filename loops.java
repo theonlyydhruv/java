0e40238efd3079c65dd111a2e22d359356e545f7
theonlyydhruv <dk3536578@gmail.com>
2026-06-24 02:16:42 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int a = 1; a<=n;a=a+1){
+            if (a%2 ==0) {
+                System.out.println(a);
+                
+            }
+        }
+        
+    
+       
+            
+        }
+        
+        
+}
+
